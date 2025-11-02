848d2f06cd58854160e83d9fbc48f10aea3647fe
Benoit DUMONT <benoit.dum74@gmail.com>
2025-11-02 17:30:05 +0100

    🐛 Fix | Vite config wrong path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'prod' ? '/portfoliov2/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfoliov2/' : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
